f8bfccfb30ad42a43f1a87c290649bf47353bb00
Courtney <courtneyobeirne@gmail.com>
2023-02-10 21:11:32 +0000

    dice roll displays

--- lib/widgets/dice_display.dart
+++ lib/widgets/dice_display.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:word_up/constants.dart';
+import 'package:word_up/main.dart';
 import 'package:word_up/widgets/backing_container.dart';
 import 'package:word_up/widgets/letter_box.dart';
 
-class DiceDisplay extends StatelessWidget {
+class DiceDisplay extends ConsumerWidget {
   const DiceDisplay({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Padding(
         padding: const EdgeInsets.all(16.0),
         child: BackingContainer(
@@ -15,34 +17,30 @@ class DiceDisplay extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Wrap(
               alignment: WrapAlignment.center,
-              children: const [
-                LetterBox(
+              children: List.generate(6, (index) {
+                if (ref.watch(dataProvider).isNotEmpty) {
+                  int score =
+                      ref.watch(dataProvider).last.diceRoll[index].score;
+                  return LetterBox(
+                    width: kDiceBoxWidth,
+                    letter: ref.watch(dataProvider).last.diceRoll[index].letter,
+                    colour: score == 1
+                        ? Colors.white
+                        : score == 2
+                            ? kYellowDiceColour
+                            : kOrangeDiceColour,
+                  );
+                }
+                return LetterBox(
                   width: kDiceBoxWidth,
-                  letter: 'N',
-                ),
-                LetterBox(
-                  width: kDiceBoxWidth,
-                  letter: 'T',
-                ),
-                LetterBox(
-                  width: kDiceBoxWidth,
-                  letter: 'D',
-                ),
-                LetterBox(
-                  width: kDiceBoxWidth,
-                  letter: 'P',
-                ),
-                LetterBox(
-                  width: kDiceBoxWidth,
-                  colour: kYellowDiceColour,
-                  letter: 'F',
-                ),
-                LetterBox(
-                  width: kDiceBoxWidth,
-                  colour: kOrangeDiceColour,
-                  letter: 'Z',
-                ),
-              ],
+                  letter: ' ',
+                  colour: index < 4
+                      ? Colors.white
+                      : index < 5
+                          ? kYellowDiceColour
+                          : kOrangeDiceColour,
+                );
+              }),
             ),
           ),
         ));
