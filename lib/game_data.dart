b70d1c56922843a1d3dd11db1f2e4f3c409599b4
Courtney <courtneyobeirne@gmail.com>
2023-02-12 15:12:07 +0000

    update dice letters

--- lib/game_data.dart
+++ lib/game_data.dart
@@ -1,12 +1,12 @@
 import 'package:word_up/model/dice.dart';
 
 List<Dice> dice = [
-  Dice(['N', 'R', 'C', 'D', 'M', 'N'], 1),
-  Dice(['T', 'R', 'K', 'L', 'G', 'B'], 1),
-  Dice(['S', 'M', 'T', 'D', 'F', 'H'], 1),
-  Dice(['L', 'S', 'R', 'K', 'N', 'B'], 1),
-  Dice(['J', 'W', 'P', 'V', 'M', 'N'], 2),
-  Dice(['Z', 'X', 'Q', 'V', 'K', 'W'], 3),
+  Dice(['L', 'N', 'S', 'T', 'R', 'D'], 1),
+  Dice(['L', 'N', 'S', 'T', 'R', 'D'], 1),
+  Dice(['G', 'B', 'C', 'M', 'P', 'H'], 1),
+  Dice(['G', 'B', 'C', 'M', 'P', 'H'], 1),
+  Dice(['F', 'Y', 'K', 'F', 'Y', 'K'], 2),
+  Dice(['Z', 'Q', 'J', 'X', 'W', 'V'], 3),
 ];
 
 List<String> vowels = ['A', 'E', 'I', 'O', 'U'];
