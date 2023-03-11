52929967c95dfa5e86ec0b6adebf73ee7b2cc392
Courtney <courtneyobeirne@gmail.com>
2023-03-11 15:04:30 +0000

    scorecard format changes

--- lib/widgets/score_card/score_card.dart
+++ lib/widgets/score_card/score_card.dart
@@ -13,64 +13,60 @@ class ScoreCard extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    List<ScoreCardRow> _rows = List.generate(11, (index) {
+      //if there has been a word submitted for that vowel type, find it in round list
+      Round? round = ref.read(roundListProvider).firstWhereOrNull((element) =>
+          element.vowelType ==
+          ref.read(vowelFilterDisplayProvider)[index].type);
+      return ScoreCardRow(
+        word: round == null ? '' : round.entry,
+        score: round == null ? ' ' : round.score.toString(),
+      );
+    });
+
     return SizedBox(
       height: screenHeight(context) * 0.85,
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              const Text(
-                'SCORECARD',
-                style: TextStyle(fontSize: 40.0),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            const Text(
+              'SCORECARD',
+              style: TextStyle(fontSize: 40.0),
+            ),
+            IntrinsicHeight(
+              //Vertical divider won't show unless wrapped in intrinsic height
+              child: Row(
+                children: [
+                  Column(
+                      children: ref
+                          .watch(vowelFilterDisplayProvider)
+                          .map((vowelFilter) {
+                    BoxDecor? decor;
+                    if (!vowelFilter.type.isPositive()) {
+                      decor = BoxDecor.negative;
+                    }
+                    if (vowelFilter.type == VowelCategory.wildcard) {
+                      decor = BoxDecor.multiLetter;
+                    }
+                    return LetterBox(
+                      width: kScoreCardBoxWidth,
+                      letter: vowelFilter.type.stringValue(),
+                      decor: decor,
+                    );
+                  }).toList()),
+                  const VerticalDivider(
+                    color: kBorderColour,
+                    thickness: 2.0,
+                    indent: 5.0,
+                  ),
+                  Column(
+                    children: [..._rows],
+                  ),
+                ],
               ),
-              IntrinsicHeight(
-                //Vertical divider won't show unless wrapped in intrinsic height
-                child: Row(
-                  children: [
-                    Column(
-                        children: ref
-                            .watch(vowelFilterDisplayProvider)
-                            .map((vowelFilter) {
-                      BoxDecor? decor;
-                      if (!vowelFilter.type.isPositive()) {
-                        decor = BoxDecor.negative;
-                      }
-                      if (vowelFilter.type == VowelCategory.wildcard) {
-                        decor = BoxDecor.multiLetter;
-                      }
-                      return LetterBox(
-                        width: kScoreCardBoxWidth,
-                        letter: vowelFilter.type.stringValue(),
-                        decor: decor,
-                      );
-                    }).toList()),
-                    const VerticalDivider(
-                      color: kBorderColour,
-                      thickness: 2.0,
-                      indent: 5.0,
-                    ),
-                    Column(
-                      children: List.generate(11, (index) {
-                        //if there has been a word submitted for that vowel type, find it in round list
-                        Round? round = ref
-                            .read(roundListProvider)
-                            .firstWhereOrNull((element) =>
-                                element.vowelType ==
-                                ref
-                                    .read(vowelFilterDisplayProvider)[index]
-                                    .type);
-                        return ScoreCardRow(
-                          word: round == null ? '' : round.entry,
-                          score: round == null ? ' ' : round.score.toString(),
-                        );
-                      }),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
